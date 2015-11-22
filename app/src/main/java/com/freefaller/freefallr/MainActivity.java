29f3fa95051fda771ccd20edcab935c403a110e1
Ryan Hornik <ryanhornik@gmail.com>
2015-11-21 23:24:07 -0600

    Can now login

--- app/src/main/java/com/freefaller/freefallr/MainActivity.java
+++ app/src/main/java/com/freefaller/freefallr/MainActivity.java
@@ -1,36 +1,15 @@
 package com.freefaller.freefallr;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import com.loopj.android.http.*;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
-
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.UnsupportedEncodingException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-
-import javax.net.ssl.HttpsURLConnection;
+import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
     Button login, signUp;
@@ -46,48 +25,51 @@ public class MainActivity extends AppCompatActivity {
         usernameField = (EditText) findViewById(R.id.username);
         passwordField = (EditText) findViewById(R.id.password);
 
-        login.setOnClickListener(new View.OnClickListener(){
-           @Override
-            public void onClick(View v){
-               Log.d("Debug", "This is working");
-               try {
-                   URL url = new URL("http://freefallr-dev.elasticbeanstalk.com/login");
-                   HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
-                   conn.setRequestMethod("POST");
-                   conn.setReadTimeout(10000);
-                   conn.setConnectTimeout(15000);
-                   conn.setDoInput(true);
-                   conn.setDoOutput(true);
-
-                   Uri.Builder builder = new Uri.Builder()
-                           .appendQueryParameter("username", usernameField.getText().toString())
-                           .appendQueryParameter("password", passwordField.getText().toString());
-                   String query = builder.build().getEncodedQuery();
-
-                   OutputStream os = conn.getOutputStream();
-                   BufferedWriter writer = new BufferedWriter(
-                           new OutputStreamWriter(os, "UTF-8"));
-                   writer.write(query);
-                   writer.flush();
-                   writer.close();
-                   os.close();
-
-                   conn.connect();
-                   Log.d("Debug", conn.getResponseMessage());
-               } catch (MalformedURLException e) {
-                   e.printStackTrace();
-               } catch (IOException e) {
-                   e.printStackTrace();
-               }
-
-           }
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RequestParams params = new RequestParams();
+                params.add("username", usernameField.getText().toString());
+                params.add("password", passwordField.getText().toString());
+                FreeFallrHttpClient.post("/login/", params, new FreeFallrHttpClient.LoginHandler());
+            }
         });
 
-        signUp.setOnClickListener(new View.OnClickListener(){
+        signUp.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, RegisterActivity.class));
             }
         });
+    }
+}
+
+class FreeFallrHttpClient{
+    public static final String BASE_URL = "http://freefallr-dev.elasticbeanstalk.com";
+    static AsyncHttpClient client = new AsyncHttpClient();
+
+    public static void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.get(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    public static void post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.post(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    private static String getAbsoluteUrl(String relativeUrl) {
+        return BASE_URL + relativeUrl;
+    }
+
+    static class LoginHandler extends TextHttpResponseHandler{
+
+        @Override
+        public void onSuccess(int statusCode, Header[] headers, String responseBody) {
+            Log.d("DEBUG", responseBody);
         }
+
+        @Override
+        public void onFailure(int statusCode, Header[] headers, String responseBody, Throwable error) {
+            Log.d("DEBUG", "Failure");
+        }
+    }
 }
