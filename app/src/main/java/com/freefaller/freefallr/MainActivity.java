a4de9f4edc60df83ff790967a77f51941e980184
Dang Huynh <Dang Huynh>
2015-11-21 23:58:15 -0600

    Added register capabilities.

--- app/src/main/java/com/freefaller/freefallr/MainActivity.java
+++ app/src/main/java/com/freefaller/freefallr/MainActivity.java
@@ -38,38 +38,9 @@ public class MainActivity extends AppCompatActivity {
         signUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+                Intent myIntent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(myIntent);
             }
         });
-    }
-}
-
-class FreeFallrHttpClient{
-    public static final String BASE_URL = "http://freefallr-dev.elasticbeanstalk.com";
-    static AsyncHttpClient client = new AsyncHttpClient();
-
-    public static void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
-        client.get(getAbsoluteUrl(url), params, responseHandler);
-    }
-
-    public static void post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
-        client.post(getAbsoluteUrl(url), params, responseHandler);
-    }
-
-    private static String getAbsoluteUrl(String relativeUrl) {
-        return BASE_URL + relativeUrl;
-    }
-
-    static class LoginHandler extends TextHttpResponseHandler{
-
-        @Override
-        public void onSuccess(int statusCode, Header[] headers, String responseBody) {
-            Log.d("DEBUG", responseBody);
-        }
-
-        @Override
-        public void onFailure(int statusCode, Header[] headers, String responseBody, Throwable error) {
-            Log.d("DEBUG", "Failure");
-        }
     }
 }
