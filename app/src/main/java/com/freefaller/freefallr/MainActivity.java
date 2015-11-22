5252b08ed103c05b32ac5df37c80171bea483b22
Dang Huynh <Dang Huynh>
2015-11-21 21:24:50 -0600

    Pushed files

--- app/src/main/java/com/freefaller/freefallr/MainActivity.java
+++ app/src/main/java/com/freefaller/freefallr/MainActivity.java
@@ -1,5 +1,7 @@
 package com.freefaller.freefallr;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -12,14 +14,23 @@ import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
     Button login, signUp;
@@ -31,36 +42,52 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         login = (Button) findViewById(R.id.login);
-        signUp = (Button) findViewById(R.id.login);
+        signUp = (Button) findViewById(R.id.signup);
         usernameField = (EditText) findViewById(R.id.username);
         passwordField = (EditText) findViewById(R.id.password);
 
         login.setOnClickListener(new View.OnClickListener(){
            @Override
             public void onClick(View v){
-               HttpClient httpClient = new DefaultHttpClient();
-               HttpPost httpPost = new HttpPost("http://10.0.2.2");
+               Log.d("Debug", "This is working");
+               try {
+                   URL url = new URL("http://freefallr-dev.elasticbeanstalk.com/login");
+                   HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
+                   conn.setRequestMethod("POST");
+                   conn.setReadTimeout(10000);
+                   conn.setConnectTimeout(15000);
+                   conn.setDoInput(true);
+                   conn.setDoOutput(true);
 
-               List<NameValuePair> nameValuePair = new ArrayList<NameValuePair>(2);
-               nameValuePair.add(new BasicNameValuePair("username", usernameField.toString()));
-               nameValuePair.add(new BasicNameValuePair("password", passwordField.toString()));
+                   Uri.Builder builder = new Uri.Builder()
+                           .appendQueryParameter("username", usernameField.getText().toString())
+                           .appendQueryParameter("password", passwordField.getText().toString());
+                   String query = builder.build().getEncodedQuery();
 
-               try{
-                   httpPost.setEntity(new UrlEncodedFormEntity(nameValuePair));
-               } catch (UnsupportedEncodingException e) {
-                   e.printStackTrace();
-               }
+                   OutputStream os = conn.getOutputStream();
+                   BufferedWriter writer = new BufferedWriter(
+                           new OutputStreamWriter(os, "UTF-8"));
+                   writer.write(query);
+                   writer.flush();
+                   writer.close();
+                   os.close();
 
-               try{
-                   HttpResponse response = httpClient.execute(httpPost);
-                   Log.d("Http Post response: ", response.toString());
-               } catch (ClientProtocolException e) {
+                   conn.connect();
+                   Log.d("Debug", conn.getResponseMessage());
+               } catch (MalformedURLException e) {
                    e.printStackTrace();
                } catch (IOException e) {
                    e.printStackTrace();
                }
+
            }
         });
-    }
 
+        signUp.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+            }
+        });
+        }
 }
---
+++ app/src/main/java/com/freefaller/freefallr/RegisterActivity.java
@@ -0,0 +1,53 @@
+package com.freefaller.freefallr;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    EditText email, username, password, confirm_password;
+    Button register;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        register = (Button) findViewById(R.id.signup);
+        email = (EditText) findViewById(R.id.email);
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        confirm_password = (EditText) findViewById(R.id.confirm_password);
+
+        register.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+            }
+        });
+
+
+    }
+
+}
